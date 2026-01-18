22fb6d7107d87624c8b6b4706483be7bb906b51f
Kirky-X <kirky-x@outlook.com>
2026-01-18 16:01:16 +0800

    style: add trailing newlines to test files
    - Add missing newline at end of file for all integration test files
    - ban_manager, cache, circuit_breaker, governor
    - limiters, matchers, quota, storage integration tests

--- tests/modules/governor/integration.rs
+++ tests/modules/governor/integration.rs
@@ -11,7 +11,7 @@ async fn test_governor_module_import() {
     let storage = std::sync::Arc::new(MemoryStorage::new());
     #[allow(unused_variables)]
     let ban_storage = std::sync::Arc::new(MemoryStorage::new());
-    
+
     // 由于 FlowControlConfig 是私有的，我们无法直接创建 Governor
     // 这里只测试导入是否正常
 }

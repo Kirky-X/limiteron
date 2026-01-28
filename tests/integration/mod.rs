02df3f58ea3ee99143b00147894399b1b8ee6496
Kirky-X <kirky-x@outlook.com>
2026-01-28 16:43:43 +0800

    feat: add cache service example

--- tests/integration/mod.rs
+++ tests/integration/mod.rs
@@ -5,13 +5,7 @@
 #[cfg(feature = "postgres")]
 #[allow(unused_imports)]
 mod postgres_test;
-#[cfg(feature = "redis")]
-#[allow(unused_imports)]
-mod redis_test;
 
 #[cfg(feature = "postgres")]
 #[allow(unused_imports)]
 pub use postgres_test::*;
-#[cfg(feature = "redis")]
-#[allow(unused_imports)]
-pub use redis_test::*;
